cdd865b6f41e9450ea161177a0f5019909c9575a
Al-Amin Firdows <alaminfirdows@gmail.com>
2020-01-23 17:38:57 +0600

    Check demo data

--- index.php
+++ index.php
@@ -5,14 +5,47 @@ require_once 'PaddleGateway.php';
 use ThemesGrove\PaddleGateway\PaddleGateway;
 
 $config = array(
-    'paddle_vendor_id'  => 'my_id',
-    'paddle_auth_code'  => 'paddle_auth_code',
-    'paddle_public_key' => 'paddle_public_key',
+    'paddle_vendor_id'  => '107514',
+    'paddle_auth_code'  => '2bf8057aadd52aa08be1dd5ebf060dea16cc1d60f99ba712c6',
+    'paddle_public_key' => '',
 );
 
 $paymentData = array (
-    'test'  => 123, 
+    'product_id' => 123,
+            'title' => 'string',
+            'webhook_url' => 'string',
+            'prices' => array (
+                0 => 'string',
+            ),
+            'recurring_prices' => array (
+                0 => 'string',
+            ),
+            'trial_days' => 123,
+            'custom_message' => 'string',
+            'coupon_code' => 'string',
+            'discountable' => 1,
+            'image_url' => 'string',
+            'return_url' => 'string',
+            'quantity_variable' => 1,
+            'quantity' => 123,
+            'expires' => 'string (date)',
+            'affiliates' => array (
+                0 => 'string',
+            ),
+            'recurring_affiliate_limit' => 123,
+            'marketing_consent' => 'string',
+            'customer_email' => 'string (email)',
+            'customer_country' => 'string',
+            'customer_postcode' => 'string',
+            'passthrough' => 'string',
+            'vat_number' => 'string',
+            'vat_company_name' => 'string',
+            'vat_street' => 'string',
+            'vat_city' => 'string',
+            'vat_state' => 'string',
+            'vat_country' => 'string',
+            'vat_postcode' => 'string',
 );
 
 $paddleGateway = new PaddleGateway($config);
-print_r($paddleGateway->proceedPayment($paymentData));
+$paddleGateway::proceedPayment($paymentData);
